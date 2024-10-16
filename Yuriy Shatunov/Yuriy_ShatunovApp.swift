5b2d22bc07f7fb4b62f701fec2794e0cf9f35fb5
Haidarov <nozimjonhaydarov6@gmail.com>
2024-10-16 20:38:08 +0500

    Initial Commit

---
+++ Yuriy Shatunov/Yuriy_ShatunovApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yuriy_ShatunovApp.swift
+//  Yuriy Shatunov
+//
+//  Created by Haidarov N on 16/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Yuriy_ShatunovApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
