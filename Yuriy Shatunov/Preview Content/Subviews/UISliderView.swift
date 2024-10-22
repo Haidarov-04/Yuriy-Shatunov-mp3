99d4f8ce352c72932700baafe476523ab3afc532
haidarov_n <nozimjonhaydarov6@gmail.com>
2024-10-23 00:58:17 +0500

    update

---
+++ Yuriy Shatunov/Preview Content/Subviews/UISliderView.swift
@@ -0,0 +1,18 @@
+//
+//  UISliderView.swift
+//  Yuriy Shatunov
+//
+//  Created by Haidarov N on 23/10/24.
+//
+
+import SwiftUI
+
+struct UISliderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UISliderView()
+}
