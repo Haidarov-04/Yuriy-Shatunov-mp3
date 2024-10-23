f0f2a2bb60f151ed8c065ff6284d21f8127fa55b
haidarov_n <nozimjonhaydarov6@gmail.com>
2024-10-23 10:51:26 +0500

    update

---
+++ Yuriy Shatunov/Preview Content/Model/airPlay.swift
@@ -0,0 +1,18 @@
+//
+//  airPlay.swift
+//  Yuriy Shatunov
+//
+//  Created by Haidarov N on 23/10/24.
+//
+
+import SwiftUI
+
+struct airPlay: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    airPlay()
+}
