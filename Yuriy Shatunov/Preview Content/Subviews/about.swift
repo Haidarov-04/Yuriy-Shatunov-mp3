092c6597b33c0db9767a38baa1f5380be168db15
haidarov_n <nozimjonhaydarov6@gmail.com>
2024-10-24 22:12:47 +0500

    update

---
+++ Yuriy Shatunov/Preview Content/Subviews/about.swift
@@ -0,0 +1,36 @@
+//
+//  settings.swift
+//  My Player
+//
+//  Created by Haidarov N on 09/10/24.
+//
+
+import SwiftUI
+
+struct about: View {
+    var body: some View {
+        ZStack {
+            
+            LinearGradient(colors: [.purple, .orange], startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                VStack{
+                    Text("Music Player")
+                    Text("Юрий Шатуров")
+                }
+                    .font(.title)
+                    .padding()
+                    .foregroundColor(.white)
+                Spacer()
+                Text("Haidarov's Project")
+                    
+            }
+        }
+    }
+}
+
+#Preview {
+    settings()
+}
