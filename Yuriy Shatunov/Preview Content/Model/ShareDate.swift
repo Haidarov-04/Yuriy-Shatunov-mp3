15ac37f22002a948a02dd5c44f5a03dee9abd2f5
haidarov_n <nozimjonhaydarov6@gmail.com>
2024-10-30 22:37:16 +0500

    update

---
+++ Yuriy Shatunov/Preview Content/Model/ShareDate.swift
@@ -0,0 +1,18 @@
+//
+//  ShareDate.swift
+//  Yuriy Shatunov
+//
+//  Created by Haidarov N on 29/10/24.
+//
+
+import SwiftUI
+
+struct ShareDate: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShareDate()
+}
